5d814b15dd444908e68ab4e9957bed7695e57b6f
jtenhave <jeff.tenhave@gmail.com>
2017-05-07 16:57:22 -0700

    Fixed up set tests.

--- Sudokungfu.Test/BaseTest.cs
+++ Sudokungfu.Test/BaseTest.cs
@@ -128,21 +128,6 @@ namespace Sudokungfu.Test
                 }
             }  
         }
-
-       /* /// <summary>
-        /// Eliminates a value from a group of cells using a technique.
-        /// </summary>
-        /// <param name="technique">Technique to use.</param>
-        /// <param name="cells">Cells in the Sudoku.</param>
-        /// <param name="value">Value to eliminate.</param>
-        /// <param name="indexes">Indexes of the cells to eliminate the value from.</param>
-        public static void EliminatePossibleValues(ITechnique technique, List<Cell> cells, int value, params int[] indexes)
-        {
-            foreach (var i in indexes)
-            {
-                cells[i].EliminatePossibleValue(value, technique);
-            }
-        }*/
     }
 
     /// <summary>
--- Sudokungfu.Test/SudokuSolver/Sets/SetTest.cs
+++ Sudokungfu.Test/SudokuSolver/Sets/SetTest.cs
@@ -4,26 +4,175 @@ using System.Linq;
 
 namespace Sudokungfu.Test.SudokuSolver.Sets
 {
+    using Sudokungfu.Extensions;
+    using Sudokungfu.Model;
+    using Sudokungfu.SudokuSolver;
     using Sudokungfu.SudokuSolver.Sets;
 
     /// <summary>
-    /// Test class for <see cref="Set"/>
+    /// Test class for <see cref="Set"/>.
     /// </summary>
     [TestClass]
     public class SetTest : BaseTest
     {
-        /*[TestMethod]
+        private List<Cell> _cells;
+        private Row _row;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _cells = GetAllCells().ToList();
+            _row = new Row(_cells, 0);
+        }
+
+        [TestMethod]
         public void TestPossibleSpots()
         {
-            var testValue = 6;
-            var cells = GetAllCells().ToList();
-            var row = new Row(cells, 0);
+            var expectedValue = 6;
             var expectedIndexes = new List<int>() { 2, 5, 6, 8 };
 
-            var technique = new TestModel();
-            EliminatePossibleValues(technique, cells, testValue, 0, 1, 3, 4, 7);
+            EliminateValues(expectedValue, new TestModel(), 0, 1, 3, 4, 7);
 
-            Assert.IsTrue(expectedIndexes.SequenceEqual(row.PossibleSpots[testValue].Select(c => c.Index)));
-        }*/
+            AssertSetEqual(expectedIndexes, _row.PossibleSpots[expectedValue].Indexes());
+        }
+
+        [TestMethod]
+        public void TestFindMinTechniquesComplexityFilter()
+        {
+            var expectedValue = 6;
+            var expectedTechniqueA = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 0, 1 }
+            };
+
+            var expectedTechniqueB = new TestModel()
+            {
+                Complexity = 1,
+                AffectedIndexes = new int[] { 0, 1 }
+            };
+
+            EliminateValues(expectedValue, expectedTechniqueA, 0);
+            EliminateValues(expectedValue, expectedTechniqueB, 1);
+
+            var actualTechqniues = _row.FindMinTechniques(Enumerable.Empty<Cell>(), expectedValue);
+            AssertSetEqual(expectedTechniqueA.ToEnumerable(), actualTechqniues);
+        }
+
+        [TestMethod]
+        public void TestFindMinTechniquesUniqueIndexFilter()
+        {
+            var expectedValue = 6;
+            var expectedTechniqueA = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 0, 1 }
+            };
+
+            var expectedTechniqueB = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 0, 1, 2 }
+            };
+
+            EliminateValues(expectedValue, expectedTechniqueA, 0);
+            EliminateValues(expectedValue, expectedTechniqueB, 1);
+
+            var actualTechqniues = _row.FindMinTechniques(Enumerable.Empty<Cell>(), expectedValue);
+            AssertSetEqual(expectedTechniqueB.ToEnumerable(), actualTechqniues);
+        }
+
+        [TestMethod]
+        public void TestFindMinTechniquesIndexCountFilter()
+        {
+            var expectedValue = 6;
+            var expectedTechniqueA = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 0, 1 }
+            };
+
+            var expectedTechniqueB = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 0, 1, 2 }
+            };
+
+            var expectedTechniqueC = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 2 }
+            };
+
+            EliminateValues(expectedValue, expectedTechniqueA, 0);
+            EliminateValues(expectedValue, expectedTechniqueB, 1);
+            EliminateValues(expectedValue, expectedTechniqueC, 2);
+
+            var actualTechqniues = _row.FindMinTechniques(Enumerable.Empty<Cell>(), expectedValue);
+            AssertSetEqual(expectedTechniqueB.ToEnumerable(), actualTechqniues);
+        }
+
+        [TestMethod]
+        public void TestFindMinTechniquesCoversAllIndexes()
+        {
+            var expectedValue = 6;
+            var expectedTechniqueA = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 0, 1 }
+            };
+
+            var expectedTechniqueB = new TestModel()
+            {
+                Complexity = 1,
+                AffectedIndexes = new int[] { 0, 2, 3, 4, 5 }
+            };
+
+            var expectedTechniqueC = new TestModel()
+            {
+                Complexity = 2,
+                AffectedIndexes = new int[] { 1, 6, 7, 8 }
+            };
+            var expectedTechniques = new TestModel[] { expectedTechniqueA, expectedTechniqueB, expectedTechniqueC };
+
+            EliminateValues(expectedValue, expectedTechniqueA, 0);
+            EliminateValues(expectedValue, expectedTechniqueB, 1);
+            EliminateValues(expectedValue, expectedTechniqueC, 2);
+
+            var actualTechqniues = _row.FindMinTechniques(Enumerable.Empty<Cell>(), expectedValue);
+            AssertSetEqual(expectedTechniques, actualTechqniues);
+        }
+
+        [TestMethod]
+        public void TestFindMinTechniquesIgnoresIndexesInProvidedCells()
+        {
+            var expectedValue = 6;
+            var expectedTechniqueA = new TestModel()
+            {
+                Complexity = 0,
+                AffectedIndexes = new int[] { 0 }
+            };
+
+            var expectedTechniqueB = new TestModel()
+            {
+                Complexity = 1,
+                AffectedIndexes = new int[] { 0, 1, 2, 4 }
+            };
+
+            EliminateValues(expectedValue, expectedTechniqueA, 0);
+            EliminateValues(expectedValue, expectedTechniqueB, 1);
+
+            var actualTechqniues = _row.FindMinTechniques(_cells[0].ToEnumerable(), expectedValue);
+            AssertSetEqual(expectedTechniqueB.ToEnumerable(), actualTechqniues);
+        }
+
+        private void EliminateValues(int value, ISudokuModel technique, params int[] indexes)
+        {
+            var cells = _cells.ToList();
+            foreach (int index in indexes)
+            {
+                cells[index].EliminatePossibleValue(value, technique);
+            }
+        }
     }
 }
